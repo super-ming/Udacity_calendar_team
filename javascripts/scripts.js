0ef27487152c046969f25c7abc168050fc0e3972
JaneEvans <jinjin.ge@utexas.edu>
2018-03-27 16:40:07 -0700

    Add Eventbrite API
    Call Eventbrite API to get a list of technology events in Seattle.

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -8,3 +8,26 @@ neighborhoodFilter.click(function() {
     $(this).next().toggle();
 
 });
+
+$(document).ready(function() {
+        
+        var token = 'XRO476MORTABZO23QCXJ';
+        var $events = $("#events");
+        
+        $.get('https://www.eventbriteapi.com/v3/events/search/?token='+token+'&location.address=Seattle&q=technology', function(res) {
+            if(res.events.length) {
+                var s = "<ul class='eventList'>";
+                for(var i=0;i<res.events.length;i++) {
+                    var event = res.events[i];
+                    console.dir(event);
+                    s += "<li><a href='" + event.url + "'>" + event.name.text + "</a> - " + "</li>";
+                }
+                s += "</ul>";
+                $events.html(s);
+            } else {
+                $events.html("<p>Sorry, there are no upcoming events.</p>");
+            }
+        });
+        
+        
+    });
