b07168e02c289c35800b759229ae8d8c7f4f6138
JaneEvans <jinjin.ge@utexas.edu>
2018-03-28 10:15:10 -0700

    Edit Homepage design
    Change colorful background theme to white/black/grey.

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
     var token = 'XRO476MORTABZO23QCXJ';
     var $events = $("#events");
 
-    $.get('https://www.eventbriteapi.com/v3/events/search/?token='+token+'&location.address=Seattle&q=technology', function(res) {
+    $.get('https://www.eventbriteapi.com/v3/events/search/?token='+token+'&location.address=Seattle&q=Technology&date_modified.keyword=this_week', function(res) {
         if(res.events.length) {
             var s = "<ul class='eventList'>";
             for(var i=0;i<res.events.length;i++) {
