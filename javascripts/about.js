1a6d81ff6aa344dbfa9a2e45959b62d409541337
Jacob Olson <37149623+jacobwolson@users.noreply.github.com>
2018-04-11 05:38:56 -0700

    Update about.js

--- javascripts/about.js
+++ javascripts/about.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  
+
   let person;
   let thisRow;
 
@@ -18,15 +18,20 @@ $(document).ready(function(){
     } else if (person === 'Alex Tran') {
     $('#Alex-Tran-modal').css('display', 'block');
     } else {
-    let thisRow = $(this).parent().attr('id');
-      if (thisRow === 'roster-row-1') {
-      $(this).css({'transform' : 'rotate(350deg)'});
-      } else if (thisRow === 'roster-row-2') {
-      $(this).css({'transform' : 'rotate(12deg)'});
-      } else if (thisRow === 'roster-row-3') {
-      $(this).css({'transform' : 'rotate(331deg)'});
+      if (this.hasAttribute('style')) {
+      $(this).removeAttr('style');
+      } else {
+        let thisRow = $(this).parent().attr('id');
+        if (thisRow === 'roster-row-1') {
+        $(this).css({'transform' : 'rotate(350deg)'});
+        return $(this);
+        } else if (thisRow === 'roster-row-2') {
+        $(this).css({'transform' : 'rotate(12deg)'});
+        } else if (thisRow === 'roster-row-3') {
+        $(this).css({'transform' : 'rotate(331deg)'});
+        };
       };
-    };
+   };
   });
 
   $('.about-close').on('click', function () {
